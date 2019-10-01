implemented skourascore and boxcarscore calculation, hopefully correct

anglegraph.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats
 
 trialOrders = {}
 
@@ -15,10 +17,11 @@ for r, d, f in os.walk(path):
             files.append(os.path.join(r, file))
 
 for i in range(len(files)):
-    #print(f)
+    if i > 1:
+        break
     data = pd.read_csv(files[i], sep="\t")
-    if data[data['instruction']!=" Rest"].instruction.tolist()[0] != " Focus":
-        continue
+    #if data[data['instruction']!=" Rest"].instruction.tolist()[0] != " Focus": #skipping subjects that don't focus first
+    #    continue
     #data[data['instruction']!=" Focus"].plot(kind='scatter', x='onset', y='needle_position')
     orig = data
     intermissions = data[data['instruction']==" Push Button"].index.tolist()
@@ -28,34 +31,47 @@ for i in range(len(files)):
     times = [first_scan_index] + intermissions + [first_rest_at_end]
     trialOrder = []
     for trialnum in range(12):
-        trialimes = (times[trialnum] + 1, times[trialnum + 1])
-        #print("Trial", trialnum + 1)
         this_trial = data[(times[trialnum] + 1):times[trialnum + 1]][data['feedback']=="On"]
         trialOrder += [this_trial['left_text'].tolist()[0][1:] + "-" + this_trial['right_text'].tolist()[0][1:], this_trial['instruction'].tolist()[0][1:]]
-        """
-        if this_trial['left_text'].tolist()[0][1:] == "Wandering":
-            print("Wandering")
-        else: #multiply by -1 if left_text=="Focused"
-            this_trial['needle_position'] = this_trial['needle_position'].apply(lambda x: x*-1)
-            print("Focused")
-        """
-    #print(data)
+    #correcting needle_positions for the counterbalanced polarity
     for idx, row in data.iterrows():
         if data.loc[idx, 'left_text'] == " Focused": #if focused is on left, then switch it back
             data.loc[idx, 'needle_position'] = 180 - data.loc[idx, 'needle_position']
             data.loc[idx, 'left_text'] = " Wandering"
             data.loc[idx, 'right_text'] = " Focused"
-        if data.loc[idx, 'instruction'] != " Focus":
+        if data.loc[idx, 'instruction'] != " Focus": #remove data values on rows that don't have instruction == wander
             data.loc[idx, 'needle_position'] = None
-    plt.plot(data.index, data['needle_position'], linewidth=0.75)
-    #with pd.option_context('display.max_rows', None):  # more options can be specified also
-    #    print(data)
-    #data[data['instruction']!=" Focus"].plot(kind='line', x='onset', y='needle_position')
-plt.vlines(x=data[data['instruction']==" Push Button"].index.tolist(), ymin=0, ymax=180)
-plt.show()
-#print(trialOrder)
-#remianing steps
-#color graph
-#graph all subjects
-#???
-#profit
+    #data at this point is cleaned up and reversed (for uncounterbalancing)
+    #calculating neurofeedback score for this subject
+    #print(trialOrder)
+    down_skourascores = []
+    down_boxcarscores = []
+    for trialnum in range(12):
+        if trialOrder[(trialnum * 2) + 1] == "Focus":
+            #print("Trial", trialnum, "had instruction == Focus")
+            #this_trial is the data just from the trial of trialnum
+            this_trial = data[(times[trialnum] + 1):times[trialnum + 1]][data['feedback']=="On"]
+            #print(this_trial.needle_position.values)
+            length = len(this_trial.needle_position.values)
+            #print(length, "needle_position values")
+            idealized = np.linspace(90, 90 - (length - 1), num=length)
+            #print("Idealized needle_positions")
+            #print(idealized)
+            skourascore = scipy.stats.pearsonr(this_trial.needle_position.values, idealized)[0]
+            down_skourascores += [skourascore]
+            #print("Pearson’s correlation coefficient (Skouras score)=", skourascore)
+            #print("\n")
+            boxcar = np.full(length, 0)
+            boxcar[0] = 90
+            boxcar[-1] = 90
+            #print(boxcar)
+            boxcarscore = scipy.stats.pearsonr(this_trial.needle_position.values, boxcar)
+            #print(boxcarscore)
+            down_boxcarscores += [boxcarscore]
+    #print(down_skourascores)
+    subpos = files[i].find('sub-A')
+    subjID = files[i][(subpos + 4):(subpos + 13)]
+    print(subjID, np.mean(down_skourascores), np.mean(down_boxcarscores))
+    #plt.plot(data.index, data['needle_position'], linewidth=0.75) #plots the graph similar to the figure
+#plt.vlines(x=data[data['instruction']==" Push Button"].index.tolist(), ymin=0, ymax=180)
+#plt.show()