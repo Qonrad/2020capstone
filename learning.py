834501ef4a9a4e203f5091130aa0cf9431f2953a
Conrad <cleonik@tulane.edu>
2019-10-18 17:17:10 -0500

    rip sklearn example code to try out cross validation for svr

--- learning.py
+++ learning.py
@@ -4,6 +4,7 @@ import os
 import pandas as pd
 import numpy as np
 #/Users/Conrad/Box/Conrad Leonik Capstone/data/connectivity/connectivity_matrices/resultsROI_Subject002_Condition001.mat
+"""
 path = './connectivity/connectivity_matrices'
 scores = pd.read_csv("./out.tsv", sep="\t")
 conn_key = pd.read_csv("./connectivity/conn_key")
@@ -35,44 +36,108 @@ y = scores.skourascore_down.values
 #print(X.shape)
 #print(y.shape)
 X = X[1:] #removing zeros first element
+np.save("skourascores", y)
+
 #print(X)
 #print(X.shape)
 #print(y.shape)
-
 print("Imputing")
 #imputing to remove NaN values using mean method
 from sklearn.preprocessing import Imputer
 imp = Imputer(missing_values='NaN', strategy='mean', axis=1)
 X = imp.fit_transform(X)
+np.save("imputed_matrices", X)
+exit()
 #print(X)
+"""
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+from sklearn.svm import SVR
 
-num_features = 30
+X = np.load("imputed_matrices.npy")
+y = np.load("skourascores.npy")
+
+num_features = 80
 print("Running SelectKBest feature selection.")
 print("Selecting", num_features, "features.")
 #only using top 25 features
-X_new = SelectKBest(mutual_info_regression, k=num_features).fit_transform(X, y)
-print(X_new.shape)
+X = SelectKBest(mutual_info_regression, k=num_features).fit_transform(X, y)
 
+# Split the dataset in two equal parts
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.15, random_state=0)
+
+# Set the parameters by cross-validation
+
+tuned_parameters = [{'kernel': ['poly'], 'gamma': [1e-3, 1e-4],
+                     'C': [1, 10, 100, 1000]},
+                    {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+
+scores = ['explained_variance', 'max_error', 'r2']
+
+for score in scores:
+    print("# Tuning hyper-parameters for %s" % score)
+    print()
+
+    clf = GridSearchCV(SVR(), tuned_parameters, cv=5,
+                       scoring=score)
+    clf.fit(X_train, y_train)
+
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.best_params_)
+    print()
+    print("Grid scores on development set:")
+    print()
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean, std * 2, params))
+    print()
+
+    print("Detailed classification report:")
+    print()
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+    print()
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(r2_score(y_true, y_pred))
+    print()
+
+
+"""
+
+print(X_new.shape)
+"""
+
+"""
 from sklearn.svm import SVR
 print("Attempting SVR")
-clf = SVR(kernel='poly', gamma='scale', C=1.0, epsilon=0.2, verbose=True)
-clf.fit(X_new, y)
-print(clf.get_params())
-print("R-squared =", clf.score(X_new, y))
+clf = SVR(kernel='linear', gamma='scale', C=100.0, epsilon=0.2, verbose=True)
+from sklearn.model_selection import cross_val_score
+scores = cross_val_score(clf, X_train, y_train, cv=10)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 
+#print("R-squared =", clf.score(X_test, y_test))
+"""
+"""
 print("attempting RidgeCV")
 from sklearn.linear_model import RidgeCV
-clf = RidgeCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(X_new, y)
+clf = RidgeCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(X_train, y_train)
 print(clf.coef_)
 print(np.argwhere(clf.coef_ > 0.01))
-print("R-squared =", clf.score(X_new, y))
+print("R-squared =", clf.score(X_test, y_test))
 print(clf.get_params())
 
 print("Running LassoCV.")
 from sklearn.linear_model import LassoCV
-reg = LassoCV(cv=5, random_state=0, n_jobs=4).fit(X_new, y)
+reg = LassoCV(cv=5, random_state=0, n_jobs=4).fit(X_train, y_train)
 print(reg.get_params())
-print("R-squared = ", reg.score(X_new, y))
+print("R-squared = ", reg.score(X_test, y_test))
 print("alpha = ", reg.alpha_)
 print("w =", reg.coef_)
+"""
