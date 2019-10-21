a6fb24d00b110a97c467d9e79c9dfd97408e01a3
Conrad <cleonik@tulane.edu>
2019-10-20 21:56:11 -0500

    make more unsuccessful attempts

--- read_psychometrics.py
+++ read_psychometrics.py
@@ -4,7 +4,7 @@ pmetrics = pd.read_excel("./NKI-merged demographic and assessment data_5.28.19.x
 all = pd.DataFrame()
 scores = pd.read_csv("./out.tsv", sep="\t")
 feats = pd.read_csv("Neuropsych_features.txt")
-feat_cols = []
+feat_cols = ['ID']
 for k in pmetrics.keys():
     sht = pmetrics[k]
     #drop complete duplicates
@@ -14,18 +14,14 @@ for k in pmetrics.keys():
     #adjust column names
     temp = sht.columns
     for col in temp:
-        #if 'Unnamed:' in col:
-        if col != 'ID':
-            sht = sht.rename(columns={col: sht[col][0]})
-    """
+        if 'Unnamed:' in col:
+            if pd.notna(sht[col][0]):
+                sht = sht.rename(columns={col: sht[col][0]})
     for c in sht.columns:
         for f in feats['Feature']:
-            print(f)
-            print(k)
-            print(c)
+            print(sht[c][0:10])
             if f in sht[c][0:10]:
                 feat_cols += [c]
-    """
     #select only rows for subjects that we have scores for
     sht = pd.merge(scores['ID'], sht, how='left', on='ID')
     #merge those rows onto the running dataframe
@@ -35,4 +31,5 @@ for k in pmetrics.keys():
         all = pd.merge(all, sht, how='left', on='ID')
     #print(all['ID'])
 all = all.drop_duplicates(subset='ID') #when there's a duplicate ID, taking the first row with that ID
-print(c)
+print(feat_cols)
+all[feat_cols].to_csv("full_subset.csv")
