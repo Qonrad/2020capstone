f4de8f8f0aa0c22b513e5f62660bb0c2175b854c
Conrad <cleonik@tulane.edu>
2019-11-26 12:29:16 -0600

    improve bids extraction using bids_validator tool

--- extract_bids_subset.py
+++ extract_bids_subset.py
@@ -1,6 +1,7 @@
-from shutil import copytree, ignore_patterns
+from shutil import copytree, ignore_patterns, copy2
 
 import os
+
 counter = 0
 IDs = []
 prefix = "/Volumes/G-RAID/RAW/RawDataBIDS/sub-"
@@ -19,19 +20,34 @@ for root, dirs, files in os.walk("/Volumes/G-RAID/RAW/RawDataBIDS"):
                 counter += 1
 print("total", counter, "files (with unique sub-IDs) matching the string in the code")
 print(IDs)
-#copytree("/Volumes/G-RAID/RAW/RawDataBIDS/sub-A00028185", "/Users/mac637-jbj-100/conrad/bids_subset_test/sub-A00028185")
 
 def sieve(dir, contentslist):
     resultinglist = []
     for entry in contentslist:
         #print(entry)
-        if ("." in entry) and ("DMNTRACKING" not in entry):
-            resultinglist += [entry]
+        if ("." in entry):
+            if ("DMNTRACKING" not in entry) and ("T1" not in entry):
+                resultinglist += [entry]
+        else:
+            if entry not in ["ses-NFB3", "anat", "func"]:
+                resultinglist += [entry]
     return resultinglist
+
+odirName = "/Users/mac637-jbj-100/conrad/bids_subset_test/"
+#copy specific other files necessary for bids
+addl_bids_files = ["/Volumes/G-RAID/RAW/RawDataBIDS/dataset_description.json",
+"/Volumes/G-RAID/RAW/RawDataBIDS/T1w.json",
+"/Volumes/G-RAID/RAW/RawDataBIDS/task-DMNTRACKINGTEST_bold.json",
+"/Volumes/G-RAID/RAW/RawDataBIDS/task-DMNTRACKINGTRAIN_bold.json",
+"/Volumes/G-RAID/RAW/RawDataBIDS/participants.tsv",
+"/Volumes/G-RAID/RAW/RawDataBIDS/README"]
+
+for f in addl_bids_files:
+    copy2(f, odirName)
 
 #create directories
 for id in IDs:
-    dirName = "/Users/mac637-jbj-100/conrad/bids_subset_test/" + "sub-" + id
+    dirName = odirName + "sub-" + id
     # Create target Directory if don't exist
     if not os.path.exists(dirName):
         #os.mkdir(dirName)
@@ -39,3 +55,14 @@ for id in IDs:
         copytree(prefix + id, dirName, ignore=sieve)
     else:
         print("Directory " , dirName ,  " already exists")
+
+
+for root, dirs, files in os.walk("/Users/mac637-jbj-100/conrad/bids_subset_test/"):
+    for file in files:
+        if ("DMNTRACKINGTRAIN" in file):
+            fullpath = os.path.join(root, file)
+            rev_file = file[0:file.find("task-")] + "task-" + "rest" + file[(file.find("task-") + 5):]
+            rev_fullpath = os.path.join(root, rev_file)
+            os.rename(fullpath, rev_fullpath)
+            print(fullpath)
+            print(rev_fullpath)
