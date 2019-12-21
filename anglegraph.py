8f0f04e0cd0a372dca2d69401c4bd372e01be727
Conrad <cleonik@tulane.edu>
2019-12-21 13:45:50 -0600

    Make anglegraph a bit more modular.

--- anglegraph.py
+++ anglegraph.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats
 
@@ -10,8 +9,8 @@ import os
 path = './events'
 
 
-def skourascore():
-    return
+def compute_skourascore(subject_performace, idealized_performance):
+    return scipy.stats.pearsonr(subject_performace, idealized_performance)[0]
 
 
 def find_empty_times(data):
@@ -21,6 +20,13 @@ def find_empty_times(data):
     first_rest_at_end = data[data['instruction']!=" Rest"].index.tolist()[-1] + 1
     times = [first_scan_index] + intermissions + [first_rest_at_end]
     return times
+
+def determine_trialorder(data, times):
+    trialOrder = []
+    for trialnum in range(12):
+        this_trial = data[(times[trialnum] + 1):times[trialnum + 1]][data['feedback']=="On"]
+        trialOrder += [this_trial['left_text'].tolist()[0][1:] + "-" + this_trial['right_text'].tolist()[0][1:], this_trial['instruction'].tolist()[0][1:]]
+    return trialOrder
 
 files = []
 # r=root, d=directories, f = files
@@ -36,40 +42,30 @@ for i in range(len(files)):
     subpos = files[i].find('sub-A')
     subjID = files[i][(subpos + 4):(subpos + 13)]
     print(subjID)
+    #reading events.tsv file as "data"
     data = pd.read_csv(files[i], sep="\t")
-    #if data[data['instruction']!=" Rest"].instruction.tolist()[0] != " Focus": #skipping subjects that don't focus first
-    #    continue
-    #data[data['instruction']!=" Focus"].plot(kind='scatter', x='onset', y='needle_position')
     times = find_empty_times(data)
-    trialOrder = []
-    for trialnum in range(12):
-        this_trial = data[(times[trialnum] + 1):times[trialnum + 1]][data['feedback']=="On"]
-        trialOrder += [this_trial['left_text'].tolist()[0][1:] + "-" + this_trial['right_text'].tolist()[0][1:], this_trial['instruction'].tolist()[0][1:]]
-    #correcting needle_positions for the counterbalanced polarity
-    #data at this point is cleaned up and reversed (for uncounterbalancing)
-    #calculating neurofeedback score for this subject
+    trialOrder = determine_trialorder(data, times)
     down_skourascores = []
     down_skourascores_90 = []
     up_skourascores = []
     up_skourascores_90 = []
     both_skourascores = []
     both_skourascores_90 = []
-    print(trialOrder)
-    print(times)
     for trialnum in range(12):
         #this_trial is the data just from the trial of trialnum
-        this_trial = data[(times[trialnum] + 1):times[trialnum + 1]]#[data['feedback']=="On"]
+        this_trial = data[(times[trialnum] + 1):times[trialnum + 1]]
         length = len(this_trial.needle_position.values)
-        print("Trial", trialnum + 1, "was", (length-1)*2, "seconds long and contained", length, "needle_position values.")
-        print("needle_position values")
-        print(this_trial.needle_position.values)
-        print(trialOrder[(trialnum * 2)])
+        #print("Trial", trialnum + 1, "was", (length-1)*2, "seconds long and contained", length, "needle_position values.")
+        #print("needle_position values")
+        #print(this_trial.needle_position.values)
+        #print(trialOrder[(trialnum * 2)])
         if trialOrder[(trialnum * 2) + 1] == "Focus":
             if trialOrder[(trialnum * 2)] == 'Focused-Wandering':
                 idealized = np.linspace(90, 90 + (length - 1), num=length)
             elif trialOrder[(trialnum * 2)] == 'Wandering-Focused':
                 idealized = np.linspace(90, 90 - (length - 1), num=length)
-            skourascore = scipy.stats.pearsonr(this_trial.needle_position.values, idealized)[0]
+            skourascore = compute_skourascore(this_trial.needle_position.values, idealized)
             down_skourascores += [skourascore]
             if length == 46:
                 down_skourascores_90 += [skourascore]
@@ -78,7 +74,7 @@ for i in range(len(files)):
                 idealized = np.linspace(90, 90 - (length - 1), num=length)
             elif trialOrder[(trialnum * 2)] == 'Wandering-Focused':
                 idealized = np.linspace(90, 90 + (length - 1), num=length)
-            skourascore = scipy.stats.pearsonr(this_trial.needle_position.values, idealized)[0]
+            skourascore = compute_skourascore(this_trial.needle_position.values, idealized)
             up_skourascores += [skourascore]
             if length == 46:
                 up_skourascores_90 += [skourascore]
@@ -87,7 +83,7 @@ for i in range(len(files)):
         both_skourascores += [skourascore]
         if length == 46:
             both_skourascores_90 += [skourascore]
-    print(down_skourascores)
+    #print(down_skourascores)
     outputdict['ID'] += [subjID]
     outputdict['skourascore_down'] += [np.mean(down_skourascores)]
     outputdict['skourascore_down_90'] += [np.mean(down_skourascores_90)]
@@ -95,7 +91,7 @@ for i in range(len(files)):
     outputdict['skourascore_up_90'] += [np.mean(up_skourascores_90)]
     outputdict['skourascore_both'] += [np.mean(both_skourascores)]
     outputdict['skourascore_both_90'] += [np.mean(both_skourascores_90)]
-    print(subjID, np.mean(down_skourascores))
+    #print(subjID, np.mean(down_skourascores))
 df = pd.DataFrame(outputdict)
 df = df.sort_values(by=['ID'])
 print(df)
